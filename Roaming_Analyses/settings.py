a03d1f799fc5d1f44eda624600df7e4b8127a238
yasmine <yasmine.elhakem@insat.ucar.tn>
2024-09-05 12:33:24 +0100

    deployment

--- Roaming_Analyses/settings.py
+++ Roaming_Analyses/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     # ...
     "staticfiles": {
